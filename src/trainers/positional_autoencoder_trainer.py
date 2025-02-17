8706ea2677f99ebc416ec16b9a6cfefafd794ddc
Ignacio Arroyo <axayakac@gmail.com>
2025-02-16 23:30:42 -0600

    Update positional_autoencoder_trainer.py

--- src/trainers/positional_autoencoder_trainer.py
+++ src/trainers/positional_autoencoder_trainer.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import List, Optional
 import wandb
 from tqdm import tqdm
+from .data.tsv_text2text_dataset import ColumnConfig
 
 class AutoencoderTrainer:
     def __init__(
