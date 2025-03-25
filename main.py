3e1cab0cdf82905b4a942b68ff76feda82fb6c02
Ignacio Arroyo <axayakac@gmail.com>
2025-03-25 17:19:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -305,16 +305,16 @@ def train_with_wandb():
         
         train_configs, valid_configs = setup_data_configs(train_path, valid_path)
         
-        TrainerClass = get_trainer_class(args.model_type)
+        TrainerClass = get_trainer_class(config.model_type)
         trainer = TrainerClass(
                 model_config=model_config,
                 training_config=training_config,
                 train_configs=train_configs,
                 valid_configs=valid_configs,
                 tokenizer_path=args.tokenizer_path,
-                cache_dir=args.cache_dir,
-                log_dir=args.log_dir,
-                use_wandb=args.use_wandb
+                #cache_dir=args.cache_dir,
+                log_dir=log_dir,
+                use_wandb=True
         )
         
         # Set up prediction logging dynamically
