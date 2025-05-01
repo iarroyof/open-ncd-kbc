7f50abe6c3f07ad5680557c8ff7f82eb899e2641
Ignacio Arroyo <axayakac@gmail.com>
2025-05-01 16:56:36 -0600

    Update tf_attention_gru.py

--- tf_attention_gru.py
+++ tf_attention_gru.py
@@ -91,6 +91,9 @@ def prepare_data(
 
 @keras.utils.register_keras_serializable()
 def custom_standardization(input_string):
+    strip_chars = string.punctuation
+    strip_chars = strip_chars.replace("[", "")
+    strip_chars = strip_chars.replace("]", "")
     lowercase = tf.strings.lower(input_string)
     return tf.strings.regex_replace(
         lowercase, "[%s]" % re.escape(strip_chars), "")
